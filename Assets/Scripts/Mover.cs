5a6425e6c7cd1398bff30e73a277b044bd86d4ce
Gustavo Daniel <gu_daniel_6@outlook.com>
2020-06-09 02:43:08 -0300

    PLayer moves with mouse click

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -6,11 +6,20 @@ using UnityEngine.AI;
 public class Mover : MonoBehaviour
 {
     [SerializeField] Transform target;
-
-
-    // Update is called once per frame
+    
     void Update()
     {
-        GetComponent<NavMeshAgent>().destination = target.position;
+        if (Input.GetMouseButton(0)){
+            MoveToCursor();
+        }
     }
+
+    private void MoveToCursor(){
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        bool HasHit = Physics.Raycast(ray, out hit);
+        if(HasHit){
+            GetComponent<NavMeshAgent>().destination = hit.point;
+        }
+    }    
 }
